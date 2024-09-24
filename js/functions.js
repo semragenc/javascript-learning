67e68d4ea4826542582d87b6d2f9097314975878
gencay.genc <gencay_genc@icloud.com>
2024-09-24 22:38:37 +0300

    I start learning arrays.

---
+++ js/functions.js
@@ -0,0 +1,85 @@
+console.log("Hello Functions")
+//function keywordu ile tanımlşanır.
+//Function Definitions
+//BİRİNCİ YÖNTEM
+function add(birinciSayi, ikinciSayi)  {
+    console.log("Toplam: ", birinciSayi + ikinciSayi)
+    console.log("Add Function Scope Level 2")
+}
+
+//Function Execution - Function Calling
+add(8,9);
+console.log("Global Scope Level 1")
+//Reusable Code - Clean Code
+function saveUserToken(token) {
+    localStorage.setItem("token", token);
+    //console.log(token)
+}
+
+saveUserToken("Bvtykgf359857456889578545747")
+//arguments keywordu ile daha önce parametre verilmemiş
+//bir fonksiyondan parametreler yaklanabiliyor.
+function topla() {
+    console.log(arguments)
+    const args = Array.from(arguments)
+    console.log(args)
+
+    let top = 0;
+    args.forEach((arg) => {
+        top += arg
+    })
+    console.log("Toplam: ", top)
+}
+
+topla(1, 2, 3, 4, 5)
+topla(4, 78, 94, 2, 57)
+
+//İKİNCİ YÖNTEM
+//Doğrudan arrow function ile çalışmaz.
+//Function keywordü ile tanımlanır.
+const cikar = function() {
+    const numbers = Array.from(arguments)
+
+    let cikarmaSonucu = 0;
+    numbers.forEach((number)=> {
+        cikarmaSonucu -= number
+    })
+
+    console.log("Çıkarma: ", cikarmaSonucu)
+}
+
+cikar(2,1)
+
+
+
+function ortalamaHesapla() {
+    const args = Array.from(arguments)
+
+
+    let topList = 0;
+    args.forEach((number) => {
+        topList += number
+    })
+
+    return topList/ args.length
+}
+
+
+const ortalama = ortalamaHesapla(1, 2, 3, 4, 36)
+console.log(ortalama)
+
+
+//SCOPE - Kapsam
+
+//Değiştirilebilecek şeyler global tanımlanır.
+//Heryerden erişebilecek şeyler global olmalı.
+const TIMEOUT_TIME = 1000;
+
+function checkTimeOut() {
+    const date = new Date()
+    console.log("Timeout", TIMEOUT_TIME)
+}
+checkTimeOut();
+console.log(date);//erişemiyor consolda hata veriyor.
+
+
